7093ef4e5bc8d87103d572d844b7a04318c43ce9
ydl <yangdeliang@juwang.cn>
2019-03-29 16:23:25 +0800

    feat:tree

--- sort/insert_sort.c
+++ sort/insert_sort.c
@@ -4,8 +4,51 @@
 
 #include <stdio.h>
 
-void insert_sort() {
+void insert_sort(int *arr, int l) {
+    int i, tmp, j;
+    for (i = 1; i < l; i++) {
+        if (arr[i - 1] > arr[i]) {
+            tmp = arr[i];
+            j = i;
+            while (j > 0 && tmp < arr[j - 1]) {
+                arr[j] = arr[j - 1];
+                j--;
+            }
+            arr[j] = tmp;
+        }
+    }
+}
 
+int binary_search(int *arr, int item, int l) {
+    int left, right, mid;
+    left = 0;
+    right = l - 1;
+    while (left <= right) {
+        mid = left + (right - left) / 2;
+        if (item == arr[mid]) {
+            return mid;
+        } else if (item < arr[mid]) {
+            right = mid - 1;
+        } else {
+            left = mid + 1;
+        }
+    }
+    return -1;
+}
+
+int binary_search_recursion(int *arr, int item, int left, int right) {
+    int mid = left + (right - left) / 2;
+    if (left > right) {
+        return -1;
+    } else if (item > arr[mid]) {
+        return binary_search_recursion(arr, item, mid + 1, right);
+    } else if (item < arr[mid]) {
+        return binary_search_recursion(arr, item, left, mid - 1);
+    } else if (item == arr[mid]) {
+        return mid;
+    } else {
+        return -1;
+    }
 }
 
 void bubble_sort(int *arr, int size) {
@@ -21,13 +64,46 @@ void bubble_sort(int *arr, int size) {
     }
 }
 
+void quick_sort_rc(int *arr, int left, int right) {
+    int i, j, tmp, t;
+    i = left;
+    j = right;
+    tmp = arr[left];
+
+    if (left > right) {
+        return;
+    }
+
+    while (i != j) {
+        while (arr[j] >= tmp && i < j) j--;
+        while (arr[i] <= tmp && i < j) i++;
+
+        if (i < j) {
+            t = arr[i];
+            arr[i] = arr[j];
+            arr[j] = t;
+        }
+    }
+
+    arr[left] = arr[i];
+    arr[i] = tmp;
+
+    quick_sort_rc(arr, left, i - 1);
+    quick_sort_rc(arr, i + 1, right);
+}
+
 int main(int argc, char* argv[])
 {
-    int arr[7] = {2, 3, 4, 5, 623, 234, 234};
-    bubble_sort(arr, 7);
+    int arr[] = {2, 23, 3, 4, 22, 7, 5, 623, 234, 234};
+    int len = sizeof(arr) / sizeof(int);
+    quick_sort_rc(arr, 0, len - 1);
     int i;
-    for (i = 0; i < 7; i++) {
+    for (i = 0; i < len; i++) {
         printf("arr[%d]:%d\n", i, arr[i]);
+    }
+
+    for (i = 0; i < len; i++) {
+        printf("result index: arr[%d] = %d => %d\n", i, arr[i], binary_search_recursion(arr, arr[i], 0, len));
     }
     return 0;
 }
---
+++ tree.c
@@ -0,0 +1,82 @@
+//
+// Created by Administrator on 2019/3/29.
+//
+
+#include <malloc.h>
+#include <stdio.h>
+
+typedef struct tree node;
+
+struct tree {
+    node *parent;
+    int val;
+    node *lchild;
+    node *rchild;
+};
+
+void insert_to_emtpy_node(node *tr, node *p) {
+    if (tr->val >= p->val) {
+        if (tr->lchild == NULL) {
+            tr->lchild = p;
+            p->parent = tr;
+            return;
+        } else {
+            insert_to_emtpy_node(tr->lchild, p);
+        }
+    } else if (tr->val < p->val) {
+        if (tr->rchild == NULL) {
+            tr->rchild = p;
+            p->parent = tr;
+            return;
+        } else {
+            insert_to_emtpy_node(tr->rchild, p);
+        }
+    }
+}
+
+node *insert_val(node *t, int val) {
+    node *n = (node *) malloc(sizeof(node));
+    n->parent = t;
+    n->val = val;
+    n->lchild = NULL;
+    n->rchild = NULL;
+    if (t == NULL) t = n;
+    else {
+        insert_to_emtpy_node(t, n);
+    }
+    return t;
+}
+
+void print_sort_tree(node *t) {
+    if (t == NULL) return;
+    print_sort_tree(t->lchild);
+    printf("val: %d\n", t->val);
+    print_sort_tree(t->rchild);
+}
+
+void destory_tree(node *t) {
+    if (t == NULL) return;
+    destory_tree(t->lchild);
+    printf("remove: %d\n", t->val);
+    free(t);
+    destory_tree(t->rchild);
+}
+
+int main(int argc, char* argv[]) {
+    node *tree = NULL;
+    tree = insert_val(tree, 2);
+    insert_val(tree, 21);
+    insert_val(tree, 2);
+    insert_val(tree, 23);
+    insert_val(tree, 2312123);
+    insert_val(tree, 212233);
+    insert_val(tree, 21233);
+    insert_val(tree, 233);
+    insert_val(tree, 12);
+    insert_val(tree, 12);
+    insert_val(tree, 12);
+    insert_val(tree, 12);
+    insert_val(tree, 12);
+    print_sort_tree(tree);
+    destory_tree(tree);
+}
